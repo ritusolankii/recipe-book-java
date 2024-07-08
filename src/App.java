943504c14ff1e3195a6d30e88c8caeeffe09c991
ritusolanki <sritu4517@gmail.com>
2024-07-08 23:00:01 +0530

    patterns-in-java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class App { 
+    public static void main(String[] args) {
+
+        int n = 4;
+        int m = 5;
+          
+               // outer loop 
+        for ( int i = 1; i<=4; i++){
+             for (int j = 1; j<=5; j++ ){
+
+                System.out.print("x");
+
+             }
+
+           System.out.println(); 
+        }
+
     }
 }
